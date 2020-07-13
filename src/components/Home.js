3cc1644e65e48a4a8c1fcf15a06925988c527858
Emily Bruner <bruner.emilym@gmail.com>
2020-07-13 16:15:10 -0400

    fixed font sizing

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,123 +14,30 @@ export default function Home() {
                     <span class="heading-primary--sub">Full Stack Web Developer</span>
                 </h1>
 
+
                 <a href="#section-tours" class="btn btn--white btn--animated">View Projects</a>
             </div>
         </header>
 
-        <section class="section-tours" id="section-tours">
-                <div class="u-center-text u-margin-bottom-big">
-                    <h2 class="heading-secondary">
-                        Featured Projects
-                    </h2>
-                </div>
-
-                <div class="row">
-                    <div class="col-1-of-3">
-                       <div class="card">
-                           <div class="card__side card__side--front">
-                                <div class="card__picture card__picture--1">
-                                    &nbsp;
-                                </div>
-                                <h4 class="card__heading">
-                                    <span class="card__heading-span card__heading-span--1">Kansha</span>
-                                </h4>
-                                <div class="card__details">
-                                    <ul>
-                                        <li>ReactJS</li>
-                                        <li>Redux</li>
-                                        <li>SASS</li>
-                                        <li>NodeJS</li>
-                                        <li>Postgres</li>
-                                    </ul>
-                                </div>
-                           </div>
-                           <div class="card__side card__side--back card__side--back-1">
-                                <div class="card__cta">
-                                    <div class="card__price-box">
-                                    <p class="card__price-only">Project Summary:</p>
-                                        <p class="card__price-value">A workplace recognition platform to appreciate peers through rewards and in-app messages. This was a build-on project where we completely overhauled the back end and front end to handle multiple user types and created the feature of organization accounts and administrator tools for monitoring employee activity and recognition.</p>
-                                    
-                                    </div>
-                                    <a href="http://www.kansharewards.com" class="btn btn--white">View Project</a>
-                                </div>
-                            </div>
-                       </div>
+        <main>
+        <section class="section-about">
+        <div class="u-center-text u-margin-bottom-big">
+        {/* <h2 class="heading-secondary">
+                        Exciting tours for adventurous people
+                    </h2> */}
+                    <div class="row">
+                    <div class="col-1-of-2">
+                    <h3 class="heading-tertiary u-margin-bottom-small">About</h3>
+                    <p class="paragraph">
+                            Hi, I'm Emily. I'm a full stack web developer based in Cary, NC.
+                        </p>
                     </div>
-
-
-                    <div class="col-1-of-3">
-                        <div class="card">
-                            <div class="card__side card__side--front">
-                                <div class="card__picture card__picture--2">
-                                    &nbsp;
-                                </div>
-                                <h4 class="card__heading">
-                                    <span class="card__heading-span card__heading-span--2">Lambda Bug Tracker</span>
-                                </h4>
-                                <div class="card__details">
-                                    <ul>
-                                        <li>ReactJS</li>
-                                        <li>Redux</li>
-                                        <li>SASS</li>
-                                        <li>NodeJS</li>
-                                        <li>Firebase</li>
-                                    </ul>
-                                </div>
-
-                            </div>
-                            <div class="card__side card__side--back card__side--back-2">
-                                <div class="card__cta">
-                                    <div class="card__price-box">
-                                        <p class="card__price-only">Project Summary: </p>
-                                        <p class="card__price-value">A web app that allows teams of developers to track bugs of projects they are working on. </p>
-                                    </div>
-                                    <a href="https://lambda-bug-tracker.firebaseapp.com/" class="btn btn--white">View Project</a>
-                                </div>
-                            </div>
                         </div>
-                    </div>
+        </div>
+        
+        </section>  
 
-
-                    <div class="col-1-of-3">
-                        <div class="card">
-                            <div class="card__side card__side--front">
-                                <div class="card__picture card__picture--3">
-                                    &nbsp;
-                                </div>
-                                <h4 class="card__heading">
-                                    <span class="card__heading-span card__heading-span--3">Better Professor</span>
-                                </h4>
-                                <div class="card__details">
-                                    <ul>
-                                    <li>ReactJS</li>
-                                        <li>Redux</li>
-                                        <li>SASS</li>
-                                        <li>NodeJS</li>
-                                        <li>Sqlite</li>
-                                    </ul>
-                                </div>
-
-                            </div>
-                            <div class="card__side card__side--back card__side--back-3">
-                                <div class="card__cta">
-                                    <div class="card__price-box">
-                                        <p class="card__price-only">Project Summary</p>
-                                        <p class="card__price-value">A web app that allows professors to manage deadlines and communications with their students. </p>
-                                    </div>
-                                    <a href="https://betterprofessor25.herokuapp.com/docs/" class="btn btn--white">View Project</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="u-center-text u-margin-top-huge">
-                    <a href="https://github.com/emilybruner" class="btn btn--green">View More Work on Github</a>
-                </div>
-            </section>
-
-            <section class="section-features">
+        <section class="section-features">
                 
                 <div class="row">
                     <div class="col-1-of-4">
@@ -182,7 +89,125 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-    </div>
 
+        <section class="section-tours" id="section-tours">
+                <div class="u-center-text u-margin-bottom-big">
+                    <h2 class="heading-secondary">
+                        Featured Projects
+                    </h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-1-of-3">
+                       <div class="card">
+                           <div class="card__side card__side--front">
+                                <div class="card__picture card__picture--1">
+                                    &nbsp;
+                                </div>
+                                <h4 class="card__heading">
+                                    <span class="card__heading-span card__heading-span--1">Kansha Rewards</span>
+                                </h4>
+                                <div class="card__details">
+                                    <ul>
+                                        <li>ReactJS</li>
+                                        <li>Redux</li>
+                                        <li>SASS</li>
+                                        <li>NodeJS</li>
+                                        <li>Postgres</li>
+                                    </ul>
+                                </div>
+                           </div>
+                           <div class="card__side card__side--back card__side--back-1">
+                                <div class="card__cta">
+                                    <div class="card__price-box">
+                                    <p class="card__price-only">Project Summary:</p>
+                                        <p class="card__price-value">A workplace recognition platform to appreciate peers through rewards and in-app messages. This was a build-on project where we completely overhauled the back end and front end to handle multiple user types and created the feature of organization accounts and administrator tools for monitoring employee activity and recognition.</p>
+                                    
+                                    </div>
+                                    <a href="http://www.kansharewards.com" class="btn btn--white">View Project</a>
+                                    <a href="https://github.com/emilybruner/kansha-fe" class="btn btn--white">View Code</a>
+                                </div>
+                            </div>
+                       </div>
+                    </div>
+
+
+                    <div class="col-1-of-3">
+                        <div class="card">
+                            <div class="card__side card__side--front">
+                                <div class="card__picture card__picture--2">
+                                    &nbsp;
+                                </div>
+                                <h4 class="card__heading">
+                                    <span class="card__heading-span card__heading-span--2">Lambda Bug Tracker</span>
+                                </h4>
+                                <div class="card__details">
+                                    <ul>
+                                        <li>ReactJS</li>
+                                        <li>Redux</li>
+                                        <li>SASS</li>
+                                        <li>NodeJS</li>
+                                        <li>Firebase</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <div class="card__side card__side--back card__side--back-2">
+                                <div class="card__cta">
+                                    <div class="card__price-box">
+                                        <p class="card__price-only">Project Summary: </p>
+                                        <p class="card__price-value">A web app that allows teams of developers to track bugs of projects they are working on. </p>
+                                    </div>
+                                    <a href="https://lambda-bug-tracker.firebaseapp.com/" class="btn btn--white">View Project</a>
+                                    <a href="https://github.com/Lambda-Bug-Tracker/Front-End" class="btn btn--white">View Code</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-1-of-3">
+                        <div class="card">
+                            <div class="card__side card__side--front">
+                                <div class="card__picture card__picture--3">
+                                    &nbsp;
+                                </div>
+                                <h4 class="card__heading">
+                                    <span class="card__heading-span card__heading-span--3">Better Professor</span>
+                                </h4>
+                                <div class="card__details">
+                                    <ul>
+                                    <li>ReactJS</li>
+                                        <li>Redux</li>
+                                        <li>SASS</li>
+                                        <li>NodeJS</li>
+                                        <li>Sqlite</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <div class="card__side card__side--back card__side--back-3">
+                                <div class="card__cta">
+                                    <div class="card__price-box">
+                                        <p class="card__price-only">Project Summary:</p>
+                                        <p class="card__price-value">A web app that allows professors to manage deadlines and communications with their students. </p>
+                                    </div>
+                                    <a href="https://betterprofessor25.herokuapp.com/docs/" class="btn btn--white">View Project</a>
+                                    <a href="https://github.com/bw-better-professor/backend" class="btn btn--white">View Code</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="u-center-text u-margin-top-huge">
+                    <a href="https://github.com/emilybruner" class="btn btn--green">View More Work on Github</a>
+                </div>
+            </section>
+
+   
+            </main>
+    </div>
+    
     )    
 }
