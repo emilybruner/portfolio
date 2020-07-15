199d201e3c533dd4ed41b18b2e1b16c23fa84f38
Emily Bruner <bruner.emilym@gmail.com>
2020-07-15 14:43:10 -0400

    worked on nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import './sass/_main.scss'
 import Home from './components/Home'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <nav classname="navigation">
+            <ul>Resume</ul>
+            <ul>Projects</ul>
+            <ul>Skills</ul>
+            <ul>About</ul>
+            <ul>Contact</ul>
+        </nav>
+    )
+}
+
